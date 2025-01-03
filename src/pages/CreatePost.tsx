c66b97bfefb7dc1bb618cdd0687129bd702d3144
Omer Sharivker <sharivker90@gmail.com>
2025-01-03 18:20:38 +0200

    sdsd

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -15,8 +15,7 @@ import OpenAI from 'openai';
 
 
 const openai = new OpenAI({
-    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true
+
 });
 
 const CreatePost: React.FC = () => {
